3616999e91a3b8f784789db24a0b6ee765b12d37
suraj <videowala13@gmail.com>
2026-06-19 17:06:32 +0530

    Add src/App.tsx React component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to AutoClerk</h1>
+        <p>Your app is ready to go!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
